884e91fe4840eec26c42dcffdd32b4a1d41256db
caicrs <caicrochask8@gmail.com>
2022-06-14 21:54:57 -0300

    v0.2

--- pages/components/navbar.jsx
+++ pages/components/navbar.jsx
@@ -2,13 +2,14 @@ import Button from "@mui/material/Button";
 import Input from "@mui/material/Input";
 import PersonIcon from '@mui/icons-material/Person';
 import { Container,Nav,Navbar,NavDropdown,Form,FormControl } from "react-bootstrap";
+import Image from "next/image";
 
 export default function NavbarComp() {
     return(
       <Navbar bg="light">
         <Container>
           <Navbar.Brand href="#home">
-            <img
+            <Image
               src="/fiesa_logo.svg"
               width="400"
               height="70"
