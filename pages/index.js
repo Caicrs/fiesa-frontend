97244a73df06a3581923cab7481e87df3291474e
Caicrs <96878399+Caicrs@users.noreply.github.com>
2022-06-14 21:57:45 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></link>
         <link
           rel="stylesheet"
@@ -31,17 +31,17 @@ export default function Home() {
         />
         <script
           src="https://unpkg.com/react/umd/react.production.min.js"
-          crossorigin
+          crossOrigin
         ></script>
 
         <script
           src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-          crossorigin
+          crossOrigin
         ></script>
 
         <script
           src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-          crossorigin
+          crossOrigin
         ></script>
       </Head>
 
